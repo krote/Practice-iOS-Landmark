e283daea443cedfb8be1e41e7120abed5445ca28
krote <krote0924@gmail.com>
2023-08-13 20:23:26 +0900

    change project name and add watch os app

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by to-matsushita on 2023/08/13.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
